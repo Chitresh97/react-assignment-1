b489d46df4238e31834514357bce67fe329c1be6
unknown <chitu1997@gmail.com>
2019-02-08 14:35:27 +0530

    2nd and 3rd task completed

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const UserInput=()=>{
-
+    return(
+        <div className="UserInput">
+            <input type="text"/>
+        </div>
+    )
 };
 export default UserInput;
