abb7dccf2343cbb4543f4de39432669318cb777f
unknown <chitu1997@gmail.com>
2019-02-08 15:22:35 +0530

    5th and 6th  task completed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import './App.css';
 import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
 class App extends Component {
+  state={
+    username:"Chitresh"
+  }
+
+  setnameHandler=()=>{
+    this.setState({
+      username:"cookie-monster"
+    })
+  }
   render() {
     return (
       <div className="App">
-        <UserOutput name="Chitresh"/>
+        <UserOutput name={this.state.username}/>
         <UserInput/>
         <UserOutput/>
 
